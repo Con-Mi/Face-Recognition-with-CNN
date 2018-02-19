afc14d686d183c0c005b09e71b021d887d888f37
Con-MI <aspris.marios@gmail.com>
2018-02-19 11:01:29 +0100

    Added a main file and more convolutions to the model. Need to add parameters

--- ResNet.py
+++ ResNet.py
@@ -3,15 +3,32 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-class ResidualNet(nn.Module):
+class ResidualNet_14(nn.Module):
 	def __init__(self):
 		super(ResidualNet, self).__init__()
 		# Conolutions Layers
 		self.conv1 = nn.Conv2d(in_channels = 3, out_channels = , kernel_size = 3)
 		self.conv2 = nn.Conv2d(in_channels = , out_channels = , kernel_size = 3)
-		self.pool = nn.MaxPool2d(kernel_size = stride = 1)
+		
+		self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+		
 		self.conv3 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
-		self.conv4 = nn.Conv2d(n_channels = , out_channels = , kernel_size = )
+		self.conv4 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		
+		self.conv5 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		self.conv6 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		
+		self.conv7 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		self.conv8 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		
+		self.conv9 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		self.conv10 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		
+		self.conv11 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		self.conv12 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		
+		self.conv13 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
+		self.conv14 = nn.Conv2d(in_channels = , out_channels = , kernel_size = )
 		
 		# CLassifier
 		self.fc1 = nn.Linear()
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+
+	Main Program.
+	
+	Run training commands with parser.
+	Includes training, evaluation, plotting and inference.
+	
+	
+	
+	
+	
+	
+
+
+"""
